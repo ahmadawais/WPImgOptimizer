e3d545c859d7a20c678b44bbc4d54e1635276450
Maedah Batool <maedahbatool@gmail.com>
2017-06-12 17:27:58 +0500

    FIX: Inline Docs 💯

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,7 @@
 /**
  * Gulpfile.
  *
- * Gulp with WordPress.
- *
- * Implements:
- *      1. Live reloads browser with BrowserSync.
- *      2. CSS: Sass to CSS conversion, error catching, Autoprefixing, Sourcemaps,
- *         CSS minification, and Merge Media Queries.
- *      3. JS: Concatenates & uglifies Vendor and Custom JS files.
- *      4. Images: Minifies PNG, JPEG, GIF and SVG images.
- *      5. Watches files for changes in CSS or JS.
- *      6. Watches files for changes in PHP.
- *      7. Corrects the line endings.
- *      8. InjectCSS instead of browser page reload.
- *      9. Generates .pot file for i18n and l10n.
+ * Compress Images in WordPress with Gulp. Minifies PNG, JPEG, GIF and SVG images.
  *
  * @author Ahmad Awais (https://github.com/ahmadawais), Maedah Batool (https://github.com/MaedahBatool)
  * @version 1.0.0
@@ -24,12 +12,9 @@
  *
  * Project Configuration for gulp tasks.
  *
- * In paths you can add <<glob or array of globs>>. Edit the variables as per your project requirements.
+ * In paths you can add <<glob or array of globs>>.
  */
-
-
-// Images related.
-var imagesSRC = './assets/img/raw/**/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF,svg,SVG}'; // Source folder of images which should be optimized.
+var imagesSRC         = './assets/img/raw/**/*.{png,PNG,jpg,JPG,jpeg,JPEG,gif,GIF,svg,SVG}'; // Source folder of images which should be optimized.
 var imagesDestination = './assets/img/opt/'; // Destination folder of optimized images. Must be different from the imagesSRC folder.
 
 /**
@@ -37,16 +22,12 @@ var imagesDestination = './assets/img/opt/'; // Destination folder of optimized 
  *
  * Load gulp plugins and assing them semantic names.
  */
-var gulp = require('gulp'); // Gulp of-course
-
-// Image realted plugins.
+var gulp     = require('gulp'); // Gulp of-course
 var imagemin = require('gulp-imagemin'); // Minify PNG, JPEG, GIF and SVG images with imagemin.
-
-// Utility related plugins.
-var notify = require('gulp-notify'); // Sends message notification to you
+var notify   = require('gulp-notify'); // Sends message notification to you
 
  /**
-	* Task: `images`.
+	* Task: `imgopt`.
 	*
 	* Minifies PNG, JPEG, GIF and SVG images.
 	*
@@ -56,7 +37,7 @@ var notify = require('gulp-notify'); // Sends message notification to you
 	*     3. Generates and saves the optimized images
 	*
 	* This task will run only once, if you want to run it
-	* again, do it with the command `gulp images`.
+	* again, do it with the command `gulp imgopt`.
 	*/
 gulp.task( 'imgopt', function() {
 	gulp.src( imagesSRC )
